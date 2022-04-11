ef68b2fde77b3483f05d061a5a38f5fa0fa792b3
Abdullah Al Masud <masudctg00@gmail.com>
2022-04-12 05:27:19 +0600

    firebase deployed

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -31,8 +31,9 @@
 
 /* 
 Firebase Hosting Steps
-1. npm install -g firebase-tools
-2. firebase login
-3. firebase init
-4. firebase deploy
+1. npm install -g firebase-tools (one time for your computer)
+2. firebase login (one time for your computer)
+3. firebase init (one time for each project)
+4. npm run build (everytime before deploying)
+5. firebase deploy (everytime you want to deploy)
 */
